21b1d08878e4cd6704d84d857f8f05c7018c1317
chimpdev2 <75502764+chimpdev2@users.noreply.github.com>
2021-09-05 23:05:44 -0400

    Update botfunny.py

--- botfunny.py
+++ botfunny.py
@@ -24,9 +24,10 @@ async def on_message(message):
             It uses the Tronald Dump api https://TronaldDump.io to recive the quotes.
             The bot is written in python.
             More features will be added eventually in the future.
-            GitHub: https://github.com/chimpdev2/tronald-dump-discord-bot
             Made by wesh#0870 on discord, Dm me for any issues.
             Thanks for using!
+            GitHub: https://github.com/chimpdev2/tronald-dump-discord-bot
+            Invite: https://discord.com/oauth2/authorize?client_id=884244613924265994&permissions=101376&scope=bot
             (the creator wesh#0870 does not support Donald Trump in any right,
             this bot should only be taken as a joke to make fun of Donald Trump)
             ''')

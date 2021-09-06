65ebe192e2cd59bdbc393ec58f23a339a727da91
chimpdev2 <75502764+chimpdev2@users.noreply.github.com>
2021-09-05 22:41:49 -0400

    main bot script
    the main script for running the bot

---
+++ botfunny.py
@@ -0,0 +1,40 @@
+import discord
+import os
+from tronalddump import api, parse
+from dotenv import load_dotenv
+
+load_dotenv()
+client = discord.Client()
+trump = api.TronaldDumpAPI()
+
+@client.event
+async def on_ready():
+    await client.change_presence(activity=discord.Game('>help for help'))
+
+@client.event
+async def on_message(message):
+
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('>help'):
+        await message.channel.send('''
+            Tronald Dump, A Discord Bot for some of Donald Trumps worst quotes.
+            Run >quote to recive a quote from the bot.
+            It uses the Tronald Dump api https://TronaldDump.io to recive the quotes.
+            The bot is written in python.
+            More features will be added eventually in the future.
+            GitHub: https://github.com/chimpdev2/tronald-dump-discord-bot
+            Made by wesh#0870 on discord, Dm me for any issues.
+            Thanks for using!
+            ''')
+    
+    if message.content.startswith('>quote'):
+        quote = trump.random_quote()
+        parsed = parse.Parser(quote)
+        await message.channel.send(parsed.value())
+        await message.channel.send("-Donald Trump")
+        await message.channel.send(parsed.date_appeared())
+
+TOKEN = os.getenv("TOKEN")
+client.run(TOKEN)
